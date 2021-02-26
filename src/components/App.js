bc34a1e5e15932c7cfc817dec6db1d7b3ea83964
komalsupekar <komalsupekar>
2021-02-26 15:31:34 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p data-ns-test="project-name">Helping Hand</p>
+      <p data-ns-test="project-description">Platform for providing help for senior citizens by the students</p>
+    </div>
   )
 }
 
